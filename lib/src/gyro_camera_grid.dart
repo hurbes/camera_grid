2c2f1499cd837b1fc3b36e96b05ab9d5138eef84
hurbes <banerjeearnab38@gmail.com>
2024-10-18 16:45:11 +0530

    Refactor CameraGrid for improved performance and cleaner toggle implementation

--- lib/src/gyro_camera_grid.dart
+++ lib/src/gyro_camera_grid.dart
@@ -5,7 +5,50 @@ import 'package:sensors_plus/sensors_plus.dart';
 import 'camera_grid_painter.dart';
 import 'utils/gyro_math.dart';
 
-class CameraGrid extends StatefulWidget {
+class CameraGrid extends StatelessWidget {
+  final Widget? child;
+  final Color gridColor;
+  final Color accentColor;
+  final double gridLineWidth;
+  final double movingBorderRadius;
+  final Duration morphDuration;
+  final Curve morphCurve;
+  final double snapThreshold;
+  final bool isGridEnabled;
+
+  const CameraGrid({
+    super.key,
+    this.child,
+    this.gridColor = Colors.white,
+    this.accentColor = Colors.blue,
+    this.gridLineWidth = 1.0,
+    this.movingBorderRadius = 4.0,
+    this.morphDuration = const Duration(milliseconds: 150),
+    this.morphCurve = Curves.easeOutQuad,
+    this.snapThreshold = 1.5 * pi / 180, // 1.5 degrees in radians
+    this.isGridEnabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final childParam = child ?? const SizedBox.shrink();
+    return switch (isGridEnabled) {
+      true => _ActiveGyroCameraGrid(
+          gridColor: gridColor,
+          accentColor: accentColor,
+          gridLineWidth: gridLineWidth,
+          movingBorderRadius: movingBorderRadius,
+          morphDuration: morphDuration,
+          morphCurve: morphCurve,
+          snapThreshold: snapThreshold,
+          child: childParam,
+        ),
+      false => childParam
+    };
+  }
+}
+
+class _ActiveGyroCameraGrid extends StatefulWidget {
   final Widget child;
   final Color gridColor;
   final Color accentColor;
@@ -15,23 +58,22 @@ class CameraGrid extends StatefulWidget {
   final Curve morphCurve;
   final double snapThreshold;
 
-  const CameraGrid({
-    super.key,
+  const _ActiveGyroCameraGrid({
     required this.child,
-    this.gridColor = Colors.white,
-    this.accentColor = Colors.blue,
-    this.gridLineWidth = 1.0,
-    this.movingBorderRadius = 4.0,
-    this.morphDuration = const Duration(milliseconds: 150),
-    this.morphCurve = Curves.easeOutQuad,
-    this.snapThreshold = 1.5 * pi / 180, // 1.5 degrees in radians
+    required this.gridColor,
+    required this.accentColor,
+    required this.gridLineWidth,
+    required this.movingBorderRadius,
+    required this.morphDuration,
+    required this.morphCurve,
+    required this.snapThreshold,
   });
 
   @override
-  _GyroCameraGridState createState() => _GyroCameraGridState();
+  _ActiveGyroCameraGridState createState() => _ActiveGyroCameraGridState();
 }
 
-class _GyroCameraGridState extends State<CameraGrid>
+class _ActiveGyroCameraGridState extends State<_ActiveGyroCameraGrid>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _morphAnimation;
